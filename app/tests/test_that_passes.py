15f43dea3873ed86a0196c76bac68ecde4176da1
Lucas Leite <me@lucasleite.us>
2021-09-29 20:53:40 -0400

    Add test_that_passes

--- app/tests/test_that_passes.py
+++ app/tests/test_that_passes.py
@@ -4,4 +4,4 @@ from django.test import TestCase
 class TestOne(TestCase):
     def test_that_fails(self):
         """This test shall fail"""
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
